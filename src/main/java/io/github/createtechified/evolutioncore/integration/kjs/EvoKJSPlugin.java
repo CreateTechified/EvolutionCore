851957c881282a441ea464ed0a386ec6be1f87fe
wrldmap <123031534+wrldmap@users.noreply.github.com>
2026-05-08 05:12:38 -0500

    kubejs plugin. idk if that does anything for us rn.

---
+++ src/main/java/io/github/createtechified/evolutioncore/integration/kjs/EvoKJSPlugin.java
@@ -0,0 +1,15 @@
+package io.github.createtechified.evolutioncore.integration.kjs;
+
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+
+public class EvoKJSPlugin extends KubeJSPlugin {
+    @Override
+    public void initStartup() {
+        super.initStartup();
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+}
