843809e24d651dae5514d7e4b9e9ae724e1d00f0
wrldmap <123031534+wrldmap@users.noreply.github.com>
2026-06-26 09:19:33 -0500

    1.0.34 - Add Manyullyn tool tier (for tcon).

--- src/main/java/io/github/createtechified/evolutioncore/common/registry/ModTags.java
+++ src/main/java/io/github/createtechified/evolutioncore/common/registry/ModTags.java
@@ -14,6 +14,7 @@ public class ModTags {
         public static final TagKey<Block> NEEDS_COPPER_TOOL = BlockTag("needs_copper_tool");
         public static final TagKey<Block> NEEDS_BRONZE_TOOL = BlockTag("needs_bronze_tool");
         public static final TagKey<Block> NEEDS_UNSTABLE_TOOL = BlockTag("needs_unstable_tool");
+        public static final TagKey<Block> NEEDS_MANYULLYN_TOOL = BlockTag("needs_manyullyn_tool");
         public static final TagKey<Block> MINEABLE_WITH_HAMMER = BlockTag("mineable_with_hammer");
 
         private static TagKey<Block> BlockTag(String name) {
@@ -24,6 +25,7 @@ public class ModTags {
     public static class Items {
         public static final TagKey<Item> INGOTS_BRONZE = ItemTag("ingots/bronze");
         public static final TagKey<Item> INGOTS_UNSTABLE = ItemTag("ingots/unstable");
+        public static final TagKey<Item> INGOTS_MANYULLYN = ItemTag("ingots/manyullyn");
 
         private static TagKey<Item> ItemTag(String name) {
             return ItemTags.create(new ResourceLocation(Reference.MODID, name));
--- src/main/java/io/github/createtechified/evolutioncore/common/registry/ModToolTiers.java
+++ src/main/java/io/github/createtechified/evolutioncore/common/registry/ModToolTiers.java
@@ -17,9 +17,10 @@ public class ModToolTiers {
     public static final Tier COPPER = TierSortingRegistry.registerTier(new ForgeTier(2, 191, 2.5f, 0.5f, 13, ModTags.Blocks.NEEDS_COPPER_TOOL, () -> Ingredient.of(Tags.Items.INGOTS_COPPER)), new ResourceLocation(Reference.MODID, "copper"), List.of(Tiers.STONE), List.of(Tiers.IRON));
     public static final Tier BRONZE = TierSortingRegistry.registerTier(new ForgeTier(4, 191, 2.5f, 0.5f, 13, ModTags.Blocks.NEEDS_BRONZE_TOOL, () -> Ingredient.of(ModTags.Items.INGOTS_BRONZE)), new ResourceLocation(Reference.MODID, "bronze"), List.of(Tiers.IRON), List.of(Tiers.DIAMOND));
     public static final Tier UNSTABLE = TierSortingRegistry.registerTier(new ForgeTier(6, 0, 8, 8, 20, ModTags.Blocks.NEEDS_UNSTABLE_TOOL, () -> Ingredient.of(ModTags.Items.INGOTS_UNSTABLE)), new ResourceLocation(Reference.MODID, "unstable"), List.of(Tiers.DIAMOND), List.of(Tiers.NETHERITE));
+    public static final Tier MANYULLYN = TierSortingRegistry.registerTier(new ForgeTier(9, 1250, 6.5f, 3.5f, 30, ModTags.Blocks.NEEDS_MANYULLYN_TOOL, () -> Ingredient.of(ModTags.Items.INGOTS_MANYULLYN)), new ResourceLocation(Reference.MODID, "manyullyn"), List.of(getGTNeutroniumTier()), List.of());
 
     // Make sure this is at the bottom of registry...
-    public static final List<Tier> TIERS = List.of(COPPER, BRONZE, UNSTABLE);
+    public static final List<Tier> TIERS = List.of(COPPER, BRONZE, UNSTABLE, MANYULLYN);
 
     public static Tier getGTNeutroniumTier() {
         ResourceLocation neutroniumId = new ResourceLocation("gtceu", "neutronium");
