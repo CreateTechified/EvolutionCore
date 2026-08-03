a882d8972f48e5433122d8db6de39e83716842a7
wrldmap <wrldmap@proton.me>
2026-08-03 18:24:23 -0500

    i hate seperated registry

--- src/main/java/io/github/createtechified/evolutioncore/common/registry/EvoMaterials.java
+++ src/main/java/io/github/createtechified/evolutioncore/common/registry/EvoMaterials.java
@@ -6,46 +6,44 @@ import com.gregtechceu.gtceu.api.data.chemical.material.info.MaterialIconSet;
 import io.github.createtechified.evolutioncore.EvolutionCoreMod;
 
 public class EvoMaterials {
-    public static void init() {
-        NEURALIUM = new Material.Builder(
-                EvolutionCoreMod.id("neuralium"))
-                .langValue("Neuralium")
-                .ingot()
-                .fluid()
-                .plasma()
-                .element(EvoElements.Nu)
-                .color(0xBD32C9)
-                .iconSet(MaterialIconSet.BRIGHT)
-                .flags(
-                        MaterialFlags.GENERATE_PLATE,
-                        MaterialFlags.GENERATE_DENSE,
-                        MaterialFlags.GENERATE_ROD,
-                        MaterialFlags.GENERATE_LONG_ROD,
-                        MaterialFlags.GENERATE_BOLT_SCREW,
-                        MaterialFlags.GENERATE_FRAME,
-                        MaterialFlags.GENERATE_GEAR,
-                        MaterialFlags.GENERATE_SMALL_GEAR,
-                        MaterialFlags.GENERATE_FOIL,
-                        MaterialFlags.GENERATE_RING,
-                        MaterialFlags.GENERATE_SPRING,
-                        MaterialFlags.GENERATE_SPRING_SMALL,
-                        MaterialFlags.GENERATE_FINE_WIRE,
-                        MaterialFlags.GENERATE_ROTOR
-                )
-                .buildAndRegister();
+    public static void init() {}
 
-        INFUSED_NEURALIUM = new Material.Builder(
-                EvolutionCoreMod.id("quantum_infused_neuralium"))
-                .langValue("Quantum-Infused Neuralium")
-                .gem()
-                .element(EvoElements.Nu2)
-                .color(0x756587)
-                .iconSet(MaterialIconSet.RUBY)
-                .flags(
-                        MaterialFlags.GENERATE_LENS
-                )
-                .buildAndRegister();
-    }
-    public static Material NEURALIUM;
-    public static Material INFUSED_NEURALIUM;
+    public static Material NEURALIUM = new Material.Builder(
+            EvolutionCoreMod.id("neuralium"))
+            .langValue("Neuralium")
+            .ingot()
+            .fluid()
+            .plasma()
+            .element(EvoElements.Nu)
+            .color(0xBD32C9)
+            .iconSet(MaterialIconSet.BRIGHT)
+            .flags(
+                    MaterialFlags.GENERATE_PLATE,
+                    MaterialFlags.GENERATE_DENSE,
+                    MaterialFlags.GENERATE_ROD,
+                    MaterialFlags.GENERATE_LONG_ROD,
+                    MaterialFlags.GENERATE_BOLT_SCREW,
+                    MaterialFlags.GENERATE_FRAME,
+                    MaterialFlags.GENERATE_GEAR,
+                    MaterialFlags.GENERATE_SMALL_GEAR,
+                    MaterialFlags.GENERATE_FOIL,
+                    MaterialFlags.GENERATE_RING,
+                    MaterialFlags.GENERATE_SPRING,
+                    MaterialFlags.GENERATE_SPRING_SMALL,
+                    MaterialFlags.GENERATE_FINE_WIRE,
+                    MaterialFlags.GENERATE_ROTOR
+            )
+            .buildAndRegister();
+
+    public static Material INFUSED_NEURALIUM = new Material.Builder(
+            EvolutionCoreMod.id("quantum_infused_neuralium"))
+            .langValue("Quantum-Infused Neuralium")
+            .gem()
+            .element(EvoElements.Nu2)
+            .color(0x756587)
+            .iconSet(MaterialIconSet.RUBY)
+            .flags(
+                    MaterialFlags.GENERATE_LENS
+            )
+            .buildAndRegister();
 }
